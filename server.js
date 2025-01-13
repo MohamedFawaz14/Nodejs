d9e4a69ab5359f3c96bd98c5f7ba2b5688079398
fawaz-website <mohamedfawaz.sb@gmail.com>
2025-01-13 14:08:56 +0530

    Add mongo db cluster

--- server.js
+++ server.js
@@ -1,36 +1,43 @@
 if(process.env.NODE_ENV !== 'production')
-{
-    require('dotenv').config()
-    }
+    {
+        require('dotenv').config()
+        }
+    
+    
+    const express = require('express')
+    const app = express()
+    const expressLayouts = require('express-ejs-layouts');
+    const bodyParser = require('body-parser')
+    
+    
+    const indexRouter = require('./routes/index')
+    const authorRouter = require('./routes/authors')
+    const bookRouter = require('./routes/books')
+    
+    app.set('view engine','ejs')
+    app.set('views',__dirname+'/views')
+    app.set('layout','./layouts/layout')
+    app.use(expressLayouts)
+    app.use(express.static('public'))
+    app.use(bodyParser.urlencoded({limit : '10mb',extended :false}))
+    
+    const mongoose = require('mongoose');
 
+// Use a properly declared and encoded MongoDB URI
+const MONGO_URI = "mongodb+srv://mohamedfawazsb:gN_jsmWdpZYz7%40j@cluster0.krjoy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const express = require('express')
-const app = express()
-const expressLayouts = require('express-ejs-layouts');
-const bodyParser = require('body-parser')
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('Connected to Mongoose'))
+  .catch((err) => console.error('MongoDB Connection Error:', err));
 
+const db = mongoose.connection;
+db.on('error', (error) => console.error('Database Error:', error));
 
-const indexRouter = require('./routes/index')
-const authorRouter = require('./routes/authors')
-const bookRouter = require('./routes/books')
-
-app.set('view engine','ejs')
-app.set('views',__dirname+'/views')
-app.set('layout','./layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({limit : '10mb',extended :false}))
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL)
-
-const db = mongoose.connection
-db.on('error',error => console.error(error))
-db.on('open',()=> console.log('connected to Mongoose'))
-
-app.use('/',indexRouter)
-app.use('/authors',authorRouter)
-app.use('/books',bookRouter)
-
-
-
-app.listen(process.env.PORT || 3000)
+    
+    app.use('/',indexRouter)
+    app.use('/authors',authorRouter)
+    app.use('/books',bookRouter)
+    
+    
+    
+    app.listen(process.env.PORT || 3000)
